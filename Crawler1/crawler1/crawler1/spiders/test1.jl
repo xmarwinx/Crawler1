17381fb04f06f8298b72ae58deb573755c4fe082
xmarwinx <35838622+xmarwinx@users.noreply.github.com>
2018-02-02 15:49:18 +0100

    Add files via upload

---
+++ Crawler1/crawler1/crawler1/spiders/test1.jl
@@ -0,0 +1,25 @@
+{"description": "\r\n        verkaufe einen Schreibtisch! sehr guter Zustand. sehr massiv rollbar mit Tasterturauszug preis verhandelbar\r\n    "}
+{"description": "\r\n        Verkaufe Tisch mit Milch-Glas-Platte, Alu-Gestell, 110x75cmx73cm L\u00e4nge/Breite/H\u00f6he Tisch wurde wenig benutzt, sehr guter Zustand. Preis bei Abholung, kann auch zerlegt werden, gebrauchte Ware.\r\n    "}
+{"description": "\r\n        Abmessungen: B x T x H = 160 x 80 x 70 in cm Preis pro St\u00fcck: 40,00 EUR 1 Schreibtisch ist leicht besch\u00e4digt, daher nur 20,00 EUR\r\n    "}
+{"description": "\r\n        Couchtisch mit Glasplatte und Ablagef\u00e4cher. Gr\u00f6\u00dfe: 90 x 90\r\n    "}
+{"description": "\r\n        Abmessungen: B x T x H = 160 x 80 x 70 in cm << Nur noch 1 St\u00fcck vorhanden >>\r\n    "}
+{"description": "\r\n        Holztisch\r\n    "}
+{"description": "\r\n        Tisch stand in einem Wochenendhaus und wurde kaum benutzt. Lediglich auf der Unterseite kleine L\u00f6cher von zwei kleinen Schauben (Tisch war an der Mauer angeschraubt) - sind nicht im Sichtbereich!\r\n    "}
+{"description": "\r\n        Couchtisch Birke furniert, keine Gebrauchspuren, 55x55x45 cm\r\n    "}
+{"description": "\r\n        Masse vom Tisch sind 80 breit 120 lang 74 hoch und die zwei Verl\u00e4ngerungsplatten zum ausziehen sind je 40 cm Achtung: Bei Interesse smsen oder anrufen! Keine Nachrichten! Es gilt das Eu-Recht! Besichtigung und Selbstabholung im Raum Wiener Neustadt, ca....\r\n    "}
+{"description": "\r\n        Zu verkaufen gelangt ein handgefertigter Esstisch aus unbehandeltem Qualit\u00e4tsholz. Die verwendeten Materialien sind handgeschliffen und der Tisch ist mit Liebe zum Detail gestaltet. Die Ma\u00dfe sind ca 115x80cm! Die Tischplatte besteht aus handgesch\u00e4lten...\r\n    "}
+{"description": "\r\n        IKEA ALVE -Laptop-Tisch schwarz-braun - 90x36 cm -\r\n    "}
+{"description": "\r\n        Funier geht an einer Seite runter, sonst tadellos.\r\n    "}
+{"description": "\r\n        160x80cm, kaum Gebrauchsspuren wegen Umzug, gegen Selbstabholung\r\n    "}
+{"description": "\r\n        Sch\u00f6ner esszimmertisch Oval 160cmx110cm ausgezogen auf 202cm preis vb\r\n    "}
+{"description": "\r\n        Es sind zwei St\u00fcck , leider sind beide etwas gebraucht . Die wurden aber so gekauft 125\u20ac stk. Jetzt habe ich leider keinen Platz mehr ! Es ist B Wahre deswegen waren sie billiger.100\u20ac f\u00fcr beide .\r\n    "}
+{"description": "\r\n        Tisch aus dunklem Holz; Verzierungen an den Beinen Ma\u00dfe: siehe Fotos *Privatverkauf*keine Garantie*kein Umtausch und keine R\u00fccknahme*\r\n    "}
+{"description": "\r\n        ausziehbarer Esstisch aus Holz und Steinplatten; beige/braun 1.30m L\u00e4nge; ausziehbar um ca. 0.50m *Privatverkauf*keine Garantie*kein Umtausch und keine R\u00fccknahme *\r\n    "}
+{"description": "\r\n        Dieser praktische Kaffeetisch hat ein zeitloses Design. Gefertigt aus massivem Kiefernholz, wird der Holztisch einen Hauch von rustikalem Charme in Ihr Wohnzimmer. Dieser einzigartige und originale Couchtisch wird ein echtes Gespr\u00e4chsthema sein, wenn Sie...\r\n    "}
+{"description": "\r\n        Dieser Couchtisch im industriellen Stil strahlt einen Vintage-Charme aus und wird eine zeitlose Erg\u00e4nzung zu Ihrem Zuhause sein. Der Couchtisch kann auch als Kaffeetisch, Wohnzimmertisch, Beistelltisch, Nachttisch, Pflanzenst\u00e4nder oder Telefonst\u00e4nder...\r\n    "}
+{"description": "\r\n        Dieses praktische 3 teilige Multifunktionstisch-Set ist flexibel, leicht und platzsparend. Er kann als Tapeziertisch, als Tisch f\u00fcr Ihren Marktstand, als Partytisch (f\u00fcr Essen, Getr\u00e4nke usw.) und auch als Arbeitstisch beim Heimwerken benutzt werden. Das...\r\n    "}
+{"description": "\r\n        Dieser Multifunktionstisch ist flexibel, leicht und platzsparend. Er kann als Tapeziertisch, als Tisch f\u00fcr Ihren Marktstand, als Partytisch (f\u00fcr Essen, Getr\u00e4nke usw.) und auch als Arbeitstisch beim Heimwerken benutzt werden. Dieser Tapeziertisch verf\u00fcgt...\r\n    "}
+{"description": "\r\n        Schreibtisch in Wei\u00df/Birke Robin von Ikea Ma\u00dfe: b/t/h. 110/60/73 cm und die Arbeitsfl\u00e4che ist 110x60cm gro\u00df Leichte Gebrauchsspuren Bitte nur Selbstabholung!\r\n    "}
+{"description": "\r\n        Ma\u00dfe 90 x 90 cm\r\n    "}
+{"description": "\r\n        Dieser atemberaubende Hochglanz-Couchtisch wird eine zeitlose Erg\u00e4nzung zu Ihrer Einrichtung sein. Die robuste Tischoberfl\u00e4che bietet eine sichere und stabile Oberfl\u00e4che, auf der Sie Getr\u00e4nke, Vasen, Obstschalen oder Ornamente platzieren k\u00f6nnen. Dieser...\r\n    "}
+{"description": "\r\n        Errichten Sie ein Casino in Ihrem Heim und veranstalten Sie Pokern\u00e4chte mit Ihren Freunden. Der Pokertisch ist mit einer hochqualitativen Armablage aus Kunstleder und einer extra dicken Schaumstoff Polsterung ausgestattet. Ebenfalls besitzt er 9...\r\n    "}
---
+++ Crawler1/crawler1/crawler1/spiders/willhabenCrawlerXpath.py
@@ -0,0 +1,44 @@
+import scrapy
+
+
+class QuotesSpider(scrapy.Spider):
+    name = "willhabenCrawlerXpath"
+    start_urls = [
+        'https://www.willhaben.at/iad/kaufen-und-verkaufen/marktplatz/tische-5751/',
+    ]
+
+    def parse(self, response):
+        
+
+        for quote in response.xpath('//article[@class="search-result-entry  "]'):
+            yield {
+               
+                
+                #TITLE
+                'title' : quote.xpath('.//div[@class="header w-brk"]/a/span/text()').extract(),
+                
+                #price
+                #note: propapy doesnt work cause script
+                #'price' : quote.xpath('.//div[@class="info"]').extract(),
+
+                
+                #description
+                'description' : quote.xpath('.//div[@class="description"]/text()').extract_first(),
+                
+                #adress
+                'adress' : quote.xpath('.//div[@class="address-lg w-brk-ln-1 "]/text()').extract(),
+                'date' : quote.xpath('.//div[@class="bottom-2"]/text()').extract(),
+
+                
+                
+                #image
+                'image' : quote.xpath('.//section[@class="image-section"]/a/img/@src').extract(),
+            }
+            
+
+
+
+        #next_page = response.css('li.next a::attr("href")').extract_first()
+        #if next_page is not None:
+        #    yield response.follow(next_page, self.parse)
+ 
